73aae473582d94058297ee0c61fc01d3d3507b61
JuyeonYu <remake867@gmail.com>
2019-05-07 14:58:47 +0900

    add cancil button / [reservation, change time, cancil] button hidden logic

--- WowReservation/Cell/ClassCell.swift
+++ WowReservation/Cell/ClassCell.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ClassCell: UITableViewCell {
+    var isReservation: Bool = false
+
     @IBOutlet weak var trainerProfile: UIImageView!
     @IBOutlet weak var trainerName: UILabel!
     @IBOutlet weak var classTitle: UILabel!
@@ -17,17 +19,24 @@ class ClassCell: UITableViewCell {
     
     @IBOutlet weak var changeTimeButton: UIButton!
     @IBOutlet weak var reservationButton: UIButton!
+    @IBOutlet weak var cancilReservationButton: UIButton!
     
     weak var classCellDelegate: ClassCellDelegate?
     
     @IBAction func didTabChangeTime(_ sender: Any) {
         print("didTabChangeTime")
+        classCellDelegate?.didTabChangeTime(cell: self)
     }
     @IBAction func didTabReservation(_ sender: Any) {
         classCellDelegate?.didTabReservation(cell: self)
+    }
+    @IBAction func didTabCancilReservation(_ sender: Any) {
+        classCellDelegate?.didTabCancilReservation(cell: self)
     }
 }
 
 protocol ClassCellDelegate: class {
     func didTabReservation(cell: ClassCell)
+    func didTabCancilReservation(cell: ClassCell)
+    func didTabChangeTime(cell: ClassCell)
 }
--- WowReservation/MainTabs/HomeController.swift
+++ WowReservation/MainTabs/HomeController.swift
@@ -24,7 +24,7 @@ class HomeController: UIViewController {
 //         주간 모드로 변경
 //        calendar.scope = .week
 //        calendar.allowsMultipleSelection = true;
-            }
+    }
     
     @IBAction func didTabTodaysWork(_ sender: Any) {
     }
@@ -44,6 +44,8 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
         cell.classTitle.text = "지옥에서 온 다이어트 PT"
         cell.classTime.text = "PM 8:30 ~ 9:30"
         cell.trainerProfile.image = UIImage(named: "profile")
+        cell.changeTimeButton.isHidden = true
+        cell.cancilReservationButton.isHidden = true
         return cell
     }
     
@@ -57,12 +59,36 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
 }
 
 extension HomeController: ClassCellDelegate {
+    func didTabChangeTime(cell: ClassCell) {
+        print("didTabChangeTime")
+    }
+    
+    func didTabCancilReservation(cell: ClassCell) {
+        let alert = UIAlertController(title: "예약을 취소할까요?", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "네", style: UIAlertAction.Style.default){ (action: UIAlertAction) -> Void in
+            cell.isReservation = false
+            cell.cancilReservationButton.isHidden = true
+            cell.changeTimeButton.isHidden = true
+            cell.reservationButton.isHidden = false
+            
+        })
+        alert.addAction(UIAlertAction(title: "아니오", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+
+    }
+    
     func didTabReservation(cell: ClassCell) {
         let alert = UIAlertController(title: "예약할까요?", message: "", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "네", style: UIAlertAction.Style.default){ (action: UIAlertAction) -> Void in
             print("yes")
+            cell.isReservation = true
+            cell.cancilReservationButton.isHidden = false
+            cell.changeTimeButton.isHidden = false
+            cell.reservationButton.isHidden = true
+            
         })
         alert.addAction(UIAlertAction(title: "아니오", style: .cancel, handler: nil))
         self.present(alert, animated: true)
     }
+    
 }
