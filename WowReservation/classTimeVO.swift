e8dd743df9d26a733f00e3ca679dd4ef9f23d93d
JuyeonYu <remake867@gmail.com>
2019-05-08 11:15:56 +0900

    Revert "specify class time VO"
    This reverts commit 08cf9365a30f085a944c8818eb5e64934cfa2651.

--- WowReservation/classTimeVO.swift
+++ WowReservation/classTimeVO.swift
@@ -10,14 +10,5 @@ import Foundation
 
 class ClassTimeVO {
     var title: String?
-    var participatingMember: Int?
-    var limitMember: Int?
-
-    var trainerProfileURL: String?
-    var trainerName: String?
-    
-    var startTime: Double?
-    var endTime: Double?
-
     var isReservation: Bool = false
 }
