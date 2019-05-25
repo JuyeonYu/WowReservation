95e78f1518885c215057279f56815f1386e5c3db
JuyeonYu <remake867@gmail.com>
2019-05-25 19:30:32 +0900

    add class enum: will reserve, did reserve, did not reserve

---
+++ WowReservation/Model/ClassEnum.swift
@@ -0,0 +1,14 @@
+//
+//  ClassEnum.swift
+//  WowReservation
+//
+//  Created by 주연  유 on 25/05/2019.
+//  Copyright © 2019 ucware. All rights reserved.
+//
+
+import Foundation
+enum classKind {
+    case willReserve
+    case didReserve
+    case didNotReserve
+}
--- WowReservation/Model/classModel.swift
+++ WowReservation/Model/classModel.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 class ClassModel {
+    
+    var classKind: classKind?
+
     var title: String?
     var participatingMember: Int?
     var limitMember: Int?
